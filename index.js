b8708a5955ae6fb5470dbe841423da2fb642c0da
Tyrone Scott <tdscottjr@gmail.com>
2021-07-09 15:00:43 -0400

    Add files via upload
    Randomize superhero/supervillian names

---
+++ index.js
@@ -0,0 +1,13 @@
+const superheroes = require('superheroes');
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+//******************//
+
+const supervillains = require('supervillains');
+
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperVillainName);
